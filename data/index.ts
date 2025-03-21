58dbd28133dac7d72c6d520ea77cae54acdb1072
OwDevon <d.legaleri@gmail.com>
2025-03-21 19:43:54 +0100

    Premier commit

---
+++ data/index.ts
@@ -0,0 +1,120 @@
+export const navItems = [
+  { name: "À propos", link: "#about" },
+  { name: "Projets", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title:
+      "Je privilégie l’échange et la collaboration pour apprendre et évoluer",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Toujours prêt à apprendre et à coder dans un monde en mouvement.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Technologies",
+    description: "J'apprends en continu.",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Passionné par la tech et en quête d’expérience",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+  {
+    id: 5,
+    title: "Actuellement en train d’explorer l’animation en JavaScript",
+    description: "Mes expérimentations en cours",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "À la recherche d’une opportunité pour apprendre et contribuer !",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 2,
+    title: "Yoom - Appli de vidéo conférence",
+    des: "Création d'une application de video conférence.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "https://www.youtube.com",
+  },
+  {
+    id: 3,
+    title: "Nerozix",
+    des: "Design du site Nerozix",
+    img: "/p2.png",
+    iconLists: ["/re.svg", "/tail.svg", "/gsap.svg"],
+    link: "(en cours)",
+  },
+  {
+    id: 4,
+    title: "Brainwave",
+    des: "Création de l'interface d'un site de chatbot IA.",
+    img: "p3.png",
+    iconLists: ["/re.svg", "/tail.svg", "/three.svg", "/gsap.svg"],
+    link: "https://brainwave-devon.vercel.app",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "CS50 – Introduction à l’informatique (Harvard, en libre accès)",
+    desc: "Formation sur les bases de l'algorithmie, de C et de python.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Formation développeur fullstack - 3WA",
+    desc: "Formation intensive en programmation de 3 mois chez 3WA (html, css, js, php, sql, etc)",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
